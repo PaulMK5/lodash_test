fd2d1b9c8ca7ea0a8d5e28ef324b54230b83a7dc
Paul <eublefar@gmail.com>
2022-09-27 10:12:25 +0300

    lodash test

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const _ = require('lodash');
+
+console.log(_.random(1, 10));
+
+/* const fs = require('fs').promises;
+
+fs.readFile('./text1.txt', 'utf-8').then(content => {
+  const newContent = `old content: ${content}`;
+});
+
+fs.appendFile('./text1.txt', 'new new new', 'UTF-8'); */
+
+// console.log(__filename);
+// console.log(__dirname);
+
+/* const { MyMath } = require('./MyMath.js');
+const {
+  utils: { func, func2 }
+} = require('./Utils');
+
+console.log(MyMath.sum(2, 2));
+console.log(MyMath.multy(3, 2)); */
